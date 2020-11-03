17738a24fe834096eb91ec1e2de45f7977750632
Cedric Rochat <rochatc@gmail.com>
2020-11-03 19:17:51 +0100

    Initial commit

---
+++ code/reboot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import cscs_pollux_env
+
+pollux = cscs_pollux_env.Pollux()
+pollux.connect()
+pollux.select_project()
+
+servers = pollux.get_server_status_list()
+for server in servers:
+    print('Server <%s>%s: %s' %(server['name'], server['msg_spacer'], server['msg']))
+
+print('Press ENTER to initiate reboot on crashed servers, or CTRL-C to cancel')
+key = pollux.wait_key()
+if key != os.linesep:
+    sys.exit()
+
+for server in servers:
+    if server['fail']:
+        print('Initiating HARD reboot on server <%s>...' %(server['name']))
+        #server.reboot(reboot_type='HARD')
