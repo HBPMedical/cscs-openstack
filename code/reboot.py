4baff53bc198df561807365155bb22e462422828
Cedric Rochat <rochatc@gmail.com>
2020-11-09 10:58:10 +0100

    Enhanced reboot script to make it more interactive

--- code/reboot.py
+++ code/reboot.py
@@ -10,14 +10,36 @@ pollux.select_project()
 
 servers = pollux.get_server_status_list()
 for server in servers:
-    print('Server <%s>%s: %s' %(server['name'], server['msg_spacer'], server['msg']))
+    print('Server <%s>%s: %s (OpenStack Status: %s)' %(server['name'], server['msg_spacer'], server['msg'], server['server'].status))
 
-print('Press ENTER to initiate reboot on crashed servers, or CTRL-C to cancel')
+goahead = False
+print('Press ENTER to review crashed servers, or any other key to skip this step')
 key = pollux.wait_key()
-if key != os.linesep:
-    sys.exit()
+if key == os.linesep:
+    goahead = True
 
-for server in servers:
-    if server['fail']:
-        print('Initiating HARD reboot on server <%s>...' %(server['name']))
-        #server['server'].reboot(reboot_type='HARD')
+if goahead:
+    other_servers = []
+    for server in servers:
+        if server['fail']:
+            print('Reboot server <%s> [y/n]?' %(server['name']))
+            key = pollux.wait_key()
+            if key.lower() == 'y':
+                print('Initiating HARD reboot on server <%s>...' %(server['name']))
+                server['server'].reboot(reboot_type='HARD')
+        else:
+            other_servers.append(server)
+
+goahead = False
+print('Press ENTER to review not necessarily crashed servers, or any other key to skip this step')
+key = pollux.wait_key()
+if key == os.linesep:
+    goahead = True
+
+if goahead:
+    for server in other_servers:
+        print('Reboot server <%s> [y/n]?' %(server['name']))
+        key = pollux.wait_key()
+        if key.lower() == 'y':
+            print('Initiating HARD reboot on server <%s>...' %(server['name']))
+            server['server'].reboot(reboot_type='HARD')
