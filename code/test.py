5209c1b57e32300c6dc423572eb45c1f5233bffb
Cedric Rochat <rochatc@gmail.com>
2021-04-12 10:19:19 +0000

    Fixed test to match failed servers

--- code/test.py
+++ code/test.py
@@ -9,14 +9,13 @@ print('Connecting...')
 pollux.connect()
 print('Scoped token?: %s' %(pollux.is_scoped()))
 
-print("\nUser: %s (ID %s)" %(os.environ['OS_USERNAME'], pollux.get_user_id()))
+print("\nUser: %s (ID %s)" %(pollux.get_env()['OS_USERNAME'], pollux.get_user_id()))
 projects = pollux.get_project_list()
 projects_names = []
 for project in projects:
     projects_names.append(project.name)
 print("\nProject list:")
 print(', '.join(projects_names))
-print('Selecting project: %s' %(os.environ['OS_PROJECT_NAME']))
 pollux.select_project()
 print('Scoped token?: %s' %(pollux.is_scoped()))
 
@@ -25,7 +24,9 @@ servers = pollux.get_server_status_list()
 for details in servers:
     server = details['server']
     print('Server %s:' %(details['name']))
-    print('     Status: %s' %(server.status))
+    print('     Status (as seen by OpenStack): %s' %(server.status))
+    if details['fail']:
+        print('     Real status <FAILED>: %s' %(details['err']['error']))
     security_groups = server.list_security_group()
     security_groups_list = []
     for security_group in security_groups:
